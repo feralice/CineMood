0c517925c428c216e236f3875f6ae5336fbddda7
[jo] <jonespjunior@gmail.com>
2023-06-06 09:49:51 -0400

    versao inicial tela escolha

--- lib/telas_para_cada_botao_do_navigation/escolha_filme.dart
+++ lib/telas_para_cada_botao_do_navigation/escolha_filme.dart
@@ -8,11 +8,104 @@ class FilmeEscolha extends StatefulWidget {
 class _FilmeEscolhaState extends State<FilmeEscolha> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text(
-          style: TextStyle(fontSize: 24),
-          'Bem-vindo ao Filme Escolha!',
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Como você está se sentindo hoje?',
+              style: TextStyle(fontSize: 24),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    // Ação quando o botão for clicado (gênero selecionado)
+                  },
+                  child: const Text('Feliz'),
+                ),
+                const SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: () {
+                    // Ação quando o botão for clicado (gênero selecionado)
+                  },
+                  child: const Text('Triste'),
+                ),
+                const SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: () {
+                    // Ação quando o botão for clicado (gênero selecionado)
+                  },
+                  child: const Text('Com Raiva'),
+                ),
+                // Adicione mais botões para outros gêneros desejados
+              ],
+            ),
+            const Text(
+              'Que gênero de filme você gosta?',
+              style: TextStyle(fontSize: 24),
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    // Ação quando o botão for clicado (gênero selecionado)
+                  },
+                  child: const Text('Ação'),
+                ),
+                const SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: () {
+                    // Ação quando o botão for clicado (gênero selecionado)
+                  },
+                  child: const Text('Comédia'),
+                ),
+                const SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: () {
+                    // Ação quando o botão for clicado (gênero selecionado)
+                  },
+                  child: const Text('Drama'),
+                ),
+                // Adicione mais botões para outros gêneros desejados
+              ],
+            ),
+            const Text(
+              'Quais serviços de straming você possui?',
+              style: TextStyle(fontSize: 24),
+            ),
+            const SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    // Ação quando o botão for clicado (gênero selecionado)
+                  },
+                  child: const Text('Netflix'),
+                ),
+                const SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: () {
+                    // Ação quando o botão for clicado (gênero selecionado)
+                  },
+                  child: const Text('Disney+'),
+                ),
+                const SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: () {
+                    // Ação quando o botão for clicado (gênero selecionado)
+                  },
+                  child: const Text('Amazon Prime'),
+                ),
+                // Adicione mais botões para outros gêneros desejados
+              ],
+            ),
+          ],
         ),
       ),
     );
