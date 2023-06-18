ad4eaa8eb4505190750339128284600b32f0a9f0
[jo] <jonespjunior@gmail.com>
2023-06-18 18:45:40 -0400

    inicio tela configurações

--- lib/telas_para_cada_botao_do_navigation/perfil.dart
+++ lib/telas_para_cada_botao_do_navigation/perfil.dart
@@ -16,83 +16,183 @@ class _ProfilePageState extends State<ProfilePage> {
 
     return Scaffold(
       body: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              CircleAvatar(
-                radius: 50,
-                // aqui vamos ajeitar depois
-                backgroundImage: NetworkImage(user?.photoURL ?? ''),
-              ),
-              const SizedBox(height: 20),
-              Text(
-                '${user?.displayName}',
-                style: const TextStyle(
-                  height: 1.4,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 24,
-                ),
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  // Action when "Filmes Vistos" button is pressed
-                },
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all<Color>(
-                    const Color(0xFFE25265),
+        child: Stack(
+          children: [
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CircleAvatar(
+                    radius: 50,
+                    // aqui vamos ajeitar depois
+                    backgroundImage: NetworkImage(user?.photoURL ?? ''),
                   ),
-                ),
-                child: const Text('Filmes Vistos'),
-              ),
-              const SizedBox(height: 10),
-              ElevatedButton(
-                onPressed: () {
-                  // Action when "Filmes para Ver" button is pressed
-                },
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all<Color>(
-                    const Color(0xFFE25265),
+                  const SizedBox(height: 20),
+                  Text(
+                    '${user?.displayName}',
+                    style: const TextStyle(
+                      height: 1.4,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 24,
+                    ),
                   ),
-                ),
-                child: const Text('Filmes para Ver'),
-              ),
-              const SizedBox(height: 10),
-              ElevatedButton(
-                onPressed: () {
-                  // Action when "Filmes Vistos" button is pressed
-                },
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all<Color>(
-                    const Color(0xFFE25265),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Action when "Filmes Vistos" button is pressed
+                    },
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(
+                        const Color(0xFFE25265),
+                      ),
+                    ),
+                    child: const Text('Filmes Vistos'),
                   ),
-                ),
-                child: const Text('Configurações de Streaming'),
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  FirebaseAuth.instance.signOut().then((_) {
-                    Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const LoginPage()),
-                    );
-                  }).catchError((error) {
-                    print('Error signing out: $error');
-                  });
-                },
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all<Color>(
-                    const Color(0xFFE25265),
+                  const SizedBox(height: 10),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Action when "Filmes para Ver" button is pressed
+                    },
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(
+                        const Color(0xFFE25265),
+                      ),
+                    ),
+                    child: const Text('Filmes para Ver'),
                   ),
-                ),
-                child: const Text('Sair da conta'),
+                  const SizedBox(height: 10),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Action when "Filmes Vistos" button is pressed
+                    },
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(
+                        const Color(0xFFE25265),
+                      ),
+                    ),
+                    child: const Text('Configurações de Streaming'),
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+            Positioned(
+              top: 16,
+              right: 16,
+              child: IconButton(
+                onPressed: () {
+                  // Action when settings button is pressed
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => SettingsPage()),
+                  );
+                },
+                icon: const Icon(Icons.settings),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SettingsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Configurações'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Tela de configurações'),
+            const SizedBox(height: 60),
+            const Divider(
+              color: Color(0xFF93AECA),
+              thickness: 1,
+              indent: 16,
+              endIndent: 16,
+            ),
+            ListTile(
+              title: const Text(
+                'Editar informações',
+                style: TextStyle(fontSize: 20),
+              ),
+              trailing: const Icon(
+                Icons.arrow_forward_ios,
+                color: Colors.white,
+              ),
+              onTap: () {
+                // Tenho que fzr a lógica para editar informações
+              },
+            ),
+            const Divider(
+              color: Color(0xFF93AECA),
+              thickness: 1,
+              indent: 16,
+              endIndent: 16,
+            ),
+            ListTile(
+              title: const Text(
+                'Alterar senha e e-mail',
+                style: TextStyle(fontSize: 20),
+              ),
+              trailing: const Icon(
+                Icons.arrow_forward_ios,
+                color: Colors.white,
+              ),
+              onTap: () {
+                //  Tenho que fzr a lógica para alterar senha
+              },
+            ),
+            const Divider(
+              color: Color(0xFF93AECA),
+              thickness: 1,
+              indent: 16,
+              endIndent: 16,
+            ),
+            ListTile(
+              title: const Text(
+                'Sobre nós',
+                style: TextStyle(fontSize: 20),
+              ),
+              trailing: const Icon(
+                Icons.arrow_forward_ios,
+                color: Colors.white,
+              ),
+              onTap: () {
+                //  Tenho que fzr a lógica para a seção "Sobre nós"
+              },
+            ),
+            const Divider(
+              color: Color(0xFF93AECA),
+              thickness: 1,
+              indent: 16,
+              endIndent: 16,
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                FirebaseAuth.instance.signOut().then((_) {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => const LoginPage()),
+                  );
+                }).catchError((error) {
+                  print('Error signing out: $error');
+                });
+              },
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(
+                  const Color(0xFFE25265),
+                ),
+              ),
+              child: const Text('Sair da conta'),
+            ),
+          ],
         ),
       ),
     );
