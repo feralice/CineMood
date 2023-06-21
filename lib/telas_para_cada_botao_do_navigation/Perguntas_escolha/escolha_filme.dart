0d23296503f71cf6d16e9ceede2ed6dd44928dee
Fernanda Alice <113216913+feralice@users.noreply.github.com>
2023-06-20 23:39:25 -0400

    tela de recomendação ajustada

--- lib/telas_para_cada_botao_do_navigation/Perguntas_escolha/escolha_filme.dart
+++ lib/telas_para_cada_botao_do_navigation/Perguntas_escolha/escolha_filme.dart
@@ -1,6 +1,9 @@
 import 'dart:convert';
-import 'package:http/http.dart' as http;
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
 import '../../constantes/cores.dart';
 import '../Home/modelos/movie_model.dart';
 import 'filmes_recomendados.dart';
@@ -32,7 +35,7 @@ class _ChoiceButtonState extends State<ChoiceButton> {
       },
       style: ButtonStyle(
         backgroundColor: MaterialStateProperty.resolveWith<Color>(
-          (Set<MaterialState> states) {
+              (Set<MaterialState> states) {
             if (states.contains(MaterialState.pressed) || widget.isSelected) {
               return const Color(0xFFE25265);
             }
@@ -62,10 +65,10 @@ class FilmeEscolha extends StatefulWidget {
   const FilmeEscolha({Key? key}) : super(key: key);
 
   @override
-  _FirstQuestionScreenState createState() => _FirstQuestionScreenState();
+  _FilmeEscolhaState createState() => _FilmeEscolhaState();
 }
 
-class _FirstQuestionScreenState extends State<FilmeEscolha> {
+class _FilmeEscolhaState extends State<FilmeEscolha> {
   String? selectedMood;
 
   Map<String, Map<String, String>> moodQuestionMap = {
@@ -74,23 +77,23 @@ class _FirstQuestionScreenState extends State<FilmeEscolha> {
       'Eufórico': '28',
     },
     'Triste': {
-      'Levantar o astral': '35',
+      'Levantar o astral': '10751',
       'Continuar no fundo do poço': '18',
     },
     'Normal': {
       'Qualquer coisa': '',
     },
     'Estressado': {
-      'Relaxar': '35',
+      'Relaxar': '10751',
       'Matar alguém': '28',
     },
     'Apaixonado': {
-      'Na fossa': '35',
+      'Na fossa': '18',
       'Apaixonadinho': '10749',
     },
     'Cansado': {
-      'Se animar': '878',
-      'Apenas relaxar': '12',
+      'Se animar': '12',
+      'Apenas relaxar': '878',
     },
   };
 
@@ -100,42 +103,80 @@ class _FirstQuestionScreenState extends State<FilmeEscolha> {
     });
   }
 
-  void _navigateToSecondQuestionScreen() {
-    if (selectedMood != null) {
+  void _navigateToRecommendedMoviesScreen() async {
+    if (selectedMood == 'Normal') {
+      _recommendMovies();
+    } else {
       Navigator.push(
         context,
         MaterialPageRoute(
           builder: (context) => SecondQuestionScreen(
             mood: selectedMood!,
             moodQuestionMap: moodQuestionMap,
+            onSelectedFeeling: _recommendMovies,
           ),
         ),
       );
+    }
+  }
+
+  void _recommendMovies() async {
+    final List<Movie> recommendedMovies = await _getRecommendedMovies();
+
+    recommendedMovies.shuffle(); // Embaralhar a lista de filmes
+
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => RecommendedMoviesScreen(movies: recommendedMovies),
+      ),
+    );
+  }
+
+  Future<List<Movie>> _getRecommendedMovies() async {
+    final genreMap = moodQuestionMap[selectedMood!];
+    final genreKeys = genreMap!.keys.toList();
+
+    final apiKey = 'edd5884041916ec57223def708862cc8';
+    final baseUrl = 'https://api.themoviedb.org/3';
+    final discoverUrl = '$baseUrl/discover/movie';
+    final language = 'pt-BR'; // idioma retornado
+
+    final random = Random();
+    final randomGenreKey = genreKeys[random.nextInt(genreKeys.length)];
+    final selectedGenre = genreMap[randomGenreKey];
+
+    final queryParams = {
+      'api_key': apiKey,
+      'with_genres': selectedGenre,
+      'language': language,
+      'page': Random().nextInt(10).toString(),
+      'sort_by': 'popularity.desc',
+    };
+
+    final uri = Uri.parse(discoverUrl).replace(queryParameters: queryParams);
+
+    final response = await http.get(uri);
+    if (response.statusCode == 200) {
+      final jsonData = json.decode(response.body);
+      final List<Movie> movies = List<Movie>.from(jsonData['results'].map((x) {
+        return Movie(
+          x['id'],
+          x['popularity'] ?? 0,
+          x['title'] ?? '',
+          x['backdrop_path'] ?? '',
+          x['poster_path'] ?? '',
+          x['overview'] ?? '',
+          x['vote_average'] ?? 0.0,
+          x['description'] ?? '',
+        );
+      }));
+
+      movies.shuffle(); // Embaralhar a lista de filmes
+
+      return movies;
     } else {
-      showDialog(
-        context: context,
-        builder: (context) => AlertDialog(
-          title: const Text('Selecione um humor',
-              style: TextStyle(fontSize: 18, color: Colors.red)),
-          content: const Text(
-              'Por favor, selecione o seu humor antes de avançar.',
-              style: TextStyle(fontSize: 16, color: Colors.white)),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: const Text('OK',
-                  style: TextStyle(fontSize: 16, color: Colors.red)),
-            ),
-          ],
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10),
-          ),
-          backgroundColor: Color(0xFF1B3658),
-          elevation: 8,
-        ),
-      );
+      throw Exception('Falha ao carregar filmes recomendados');
     }
   }
 
@@ -175,7 +216,7 @@ class _FirstQuestionScreenState extends State<FilmeEscolha> {
             Align(
               alignment: const Alignment(0.8, 0),
               child: FloatingActionButton(
-                onPressed: _navigateToSecondQuestionScreen,
+                onPressed: _navigateToRecommendedMoviesScreen,
                 backgroundColor: const Color(0xFFE25265),
                 child: const Icon(Icons.arrow_forward),
               ),
@@ -190,11 +231,13 @@ class _FirstQuestionScreenState extends State<FilmeEscolha> {
 class SecondQuestionScreen extends StatefulWidget {
   final String mood;
   final Map<String, Map<String, String>> moodQuestionMap;
+  final Function() onSelectedFeeling;
 
   const SecondQuestionScreen({
     Key? key,
     required this.mood,
     required this.moodQuestionMap,
+    required this.onSelectedFeeling,
   }) : super(key: key);
 
   @override
@@ -211,89 +254,7 @@ class _SecondQuestionScreenState extends State<SecondQuestionScreen> {
   }
 
   void _recommendMovies() async {
-    if (selectedFeeling != null) {
-      final selectedGenre =
-          widget.moodQuestionMap[widget.mood]![selectedFeeling!];
-      final List<Movie> recommendedMovies =
-          await _getRecommendedMovies(selectedGenre);
-
-      recommendedMovies.shuffle(); // Embaralhar a lista de filmes
-
-      Navigator.push(
-        context,
-        MaterialPageRoute(
-          builder: (context) =>
-              RecommendedMoviesScreen(movies: recommendedMovies),
-        ),
-      );
-    } else {
-      showDialog(
-        context: context,
-        builder: (context) => AlertDialog(
-          title: const Text('Selecione um humor',
-              style: TextStyle(color: Colors.red)),
-          content:
-              const Text('Por favor, selecione o seu humor antes de avançar.',
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: Colors.white,
-                  )),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: const Text('OK',
-                  style: TextStyle(fontSize: 16, color: Colors.red)),
-            ),
-          ],
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10),
-          ),
-          backgroundColor: Color(0xFF1B3658),
-          elevation: 8,
-        ),
-      );
-    }
-  }
-
-  Future<List<Movie>> _getRecommendedMovies(String? genre) async {
-    if (genre == null || genre.isEmpty) {
-      throw Exception('Gênero não definido');
-    }
-
-    final apiKey = 'edd5884041916ec57223def708862cc8';
-    final baseUrl = 'https://api.themoviedb.org/3';
-    final discoverUrl = '$baseUrl/discover/movie';
-    final language = 'pt-BR';
-
-    final queryParams = {
-      'api_key': apiKey,
-      'with_genres': genre,
-      'language': language,
-    };
-
-    final uri = Uri.parse(discoverUrl).replace(queryParameters: queryParams);
-
-    final response = await http.get(uri);
-    if (response.statusCode == 200) {
-      final jsonData = json.decode(response.body);
-      final List<Movie> movies = List<Movie>.from(jsonData['results'].map((x) {
-        return Movie(
-          x['id'],
-          x['popularity'] ?? 0,
-          x['title'] ?? '',
-          x['backdrop_path'] ?? '',
-          x['poster_path'] ?? '',
-          x['overview'] ?? '',
-          x['vote_average'] ?? 0.0,
-          x['description'] ?? '',
-        );
-      }));
-      return movies;
-    } else {
-      throw Exception('Falha ao carregar filmes recomendados');
-    }
+    widget.onSelectedFeeling();
   }
 
   String pegaHumor() {
@@ -323,8 +284,8 @@ class _SecondQuestionScreenState extends State<SecondQuestionScreen> {
         ),
         backgroundColor: AppColors.azul,
         iconTheme: const IconThemeData(
-            color:
-                AppColors.vermelho), // Altera a cor do ícone da seta de volta
+          color: AppColors.vermelho,
+        ), // Altera a cor do ícone da seta de volta
       ),
       body: Padding(
         padding: const EdgeInsets.all(30),
@@ -346,8 +307,7 @@ class _SecondQuestionScreenState extends State<SecondQuestionScreen> {
             Wrap(
               alignment: WrapAlignment.center,
               spacing: 10,
-              children:
-                  widget.moodQuestionMap[widget.mood]!.keys.map((feeling) {
+              children: widget.moodQuestionMap[widget.mood]!.keys.map((feeling) {
                 return ChoiceButton(
                   text: feeling,
                   onPressed: _selectFeeling,
@@ -366,43 +326,6 @@ class _SecondQuestionScreenState extends State<SecondQuestionScreen> {
             ),
           ],
         ),
-      ),
-    );
-  }
-}
-
-class RecommendedMoviesScreen extends StatelessWidget {
-  final List<Movie> movies;
-
-  const RecommendedMoviesScreen({Key? key, required this.movies})
-      : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'CineMood',
-          style: TextStyle(
-            color: AppColors.vermelho,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        backgroundColor: AppColors.azul,
-        iconTheme: const IconThemeData(color: AppColors.vermelho),
-      ),
-      body: ListView.builder(
-        itemCount: movies.length,
-        itemBuilder: (context, index) {
-          final movie = movies[index];
-          return ListTile(
-            title: Text(movie.title),
-            subtitle: Text(movie.overview),
-            leading: Image.network(
-              'https://image.tmdb.org/t/p/w200${movie.poster}',
-            ),
-          );
-        },
       ),
     );
   }
--- lib/telas_para_cada_botao_do_navigation/Perguntas_escolha/filmes_recomendados.dart
+++ lib/telas_para_cada_botao_do_navigation/Perguntas_escolha/filmes_recomendados.dart
@@ -1,28 +1,196 @@
 import 'package:flutter/material.dart';
+import '../../constantes/cores.dart';
 import '../Home/modelos/movie_model.dart';
 
-class RecommendedMoviesScreen extends StatelessWidget {
+class RecommendedMoviesScreen extends StatefulWidget {
   final List<Movie> movies;
 
-  const RecommendedMoviesScreen({Key? key, required this.movies}) : super(key: key);
+  const RecommendedMoviesScreen({Key? key, required this.movies})
+      : super(key: key);
+
+  @override
+  _RecommendedMoviesScreenState createState() =>
+      _RecommendedMoviesScreenState();
+}
+
+class _RecommendedMoviesScreenState extends State<RecommendedMoviesScreen> {
+  late List<Movie>
+      _movies; // Usando a palavra-chave 'late' para inicializar posteriormente
+  int _currentIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _movies = List.from(widget.movies); // Cria uma cópia da lista original
+  }
+
+  void _removeMovie(int index) {
+    setState(() {
+      _movies.removeAt(index);
+      if (_currentIndex >= _movies.length) {
+        _currentIndex = 0; // Redefine o índice atual para o primeiro filme
+      }
+    });
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('Filme descartado'),
+        duration: Duration(seconds: 1),
+      ),
+    );
+  }
+
+  void _moveToWatched(int index) {
+    // Adicione o código para mover o filme para a lista de filmes assistidos
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('Filme movido para a lista de filmes assistidos'),
+        duration: Duration(seconds: 1),
+      ),
+    );
+  }
+
+  void _addToWatchlist(int index) {
+    // Adicione o código para adicionar o filme à watchlist
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text('Filme adicionado à watchlist'),
+        duration: Duration(seconds: 1),
+      ),
+    );
+  }
+
+  void _showNextMovie() {
+    setState(() {
+      if (_currentIndex < _movies.length - 1) {
+        _currentIndex++;
+      } else {
+        // Caso todos os filmes tenham sido mostrados, você pode tomar uma ação, como mostrar uma mensagem ou redirecionar para outra tela.
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('Todos os filmes foram mostrados'),
+            duration: Duration(seconds: 1),
+          ),
+        );
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    if (_movies.isEmpty) {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'CineMood',
+            style: TextStyle(
+              color: AppColors.vermelho,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          backgroundColor: AppColors.azul,
+          iconTheme: const IconThemeData(
+            color: AppColors.vermelho,
+          ),
+        ),
+        body: Center(
+          child: Text('Sem mais filmes para recomendar'),
+        ),
+      );
+    }
+
+    final movie = _movies[_currentIndex];
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Filmes Recomendados'),
+        title: const Text(
+          'CineMood',
+          style: TextStyle(
+            color: AppColors.vermelho,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        backgroundColor: AppColors.azul,
+        iconTheme: const IconThemeData(
+          color: AppColors.vermelho,
+        ),
       ),
-      body: ListView.builder(
-        itemCount: movies.length,
-        itemBuilder: (context, index) {
-          final movie = movies[index];
-          return ListTile(
-            leading: Image.network('https://image.tmdb.org/t/p/w500${movie.poster}')
-            ,
-            title: Text(movie.title),
-            subtitle: Text(movie.overview),
-          );
-        },
+      body: Column(
+        children: [
+          SizedBox(height: 20),
+          // ...
+
+          Card(
+            color: AppColors
+                .azul, // Adicione esta linha para definir o fundo do Card como transparente
+            child: Column(
+              children: [
+                Image.network(
+                  'https://image.tmdb.org/t/p/w200${movie.poster}',
+                  width: 150,
+                  height: 220,
+                  fit: BoxFit.cover,
+                ),
+                ListTile(
+                  tileColor:
+                      null, // fundo do ListTile como nulo
+                  title: Text(
+                    movie.title,
+                    style: const TextStyle(
+                        fontWeight: FontWeight.bold, color: Colors.white, fontSize: 20),
+                  ),
+                  subtitle: Text(
+                    movie.overview,
+                    style: const TextStyle(
+                      color: Colors
+                          .white70, // Defina a cor desejada para a descrição
+                    ),
+                  ),
+                  contentPadding: const EdgeInsets.all(8.0),
+                ),
+              ],
+            ),
+          ),
+
+// ...
+
+          SizedBox(height: 20),
+          Container(
+            width: double.infinity,
+            height: 60,
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                IconButton(
+                  icon: Icon(Icons.clear),
+                  color: Colors.red,
+                  iconSize: 30,
+                  onPressed: () {
+                    _removeMovie(_currentIndex);
+                  },
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    _moveToWatched(_currentIndex);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.blueGrey,
+                  ),
+                  child: Text('Já assisti'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    _addToWatchlist(_currentIndex);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: AppColors.verde_escuro,
+                  ),
+                  child: Text('Vou assistir'),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
